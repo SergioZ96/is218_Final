302749e46df89a26a9eaf6b37dcd0f3a025d78ac
Sergio Zurita <Sergiod.zurita@gmail.com>
2017-05-04 10:34:43 -0400

    Functions for editing, adding and deleting tasks

--- Model/task_db.php
+++ Model/task_db.php
@@ -1,9 +1,46 @@
 <?php
-function add_task($task_id,$task_name,$status,$user_name,$time)
+function add_task($task_id,$task_name,$status,$user_id,$time_made)
 {
 	global $db;
 	$query = 'INSERT INTO UsersToDo
 				(id, task, done, user, created)
 			  VALUES
-			  	(:task_id, :task_name, :status, :user_name, :time)';
+			  	(:task_id, :task_name, :status, :user_id, :time_made)';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':task_id', $task_id);
+	$statement->bindValue(':task_name', $task_name);
+	$statement->bindValue(':status', $status);
+	$statement->bindValue(':user_id', $user_id);
+	$statement->bindValue(':time_made', $time_made);
+	$statement->execute();
+	$statement->closeCursor();
 }
+
+function delete_task($task_id,$user_name)
+{
+	global $db;
+	$query = 'DELETE FROM UsersToDo
+			  WHERE id = :task_id 
+			  AND WHERE user = :user_id';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':task_id', $task_id);
+	$statement->bindValue(':user_id', $user_id);
+	$statement->execute();
+	$statement->closeCursor();
+}
+
+function edit_task($task_id,$task_name,$user_id)
+{
+	global $db;
+	$query = 'DELETE FROM UsersToDo
+			  WHERE id = :task_id
+			  AND WHERE user = :user_id';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':task_id', $task_id);
+	$statement->bindValue(':task_name', $task_name);
+	$statement->bindValue(':user_id', $user_id);
+	$statement->execute();
+	$statement->closeCursor();
+}
+
+?>
