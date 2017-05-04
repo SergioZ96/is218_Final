066abfb0a0c1b2c98ac877eefc1f798f1b5d879a
Sergio Zurita <Sergiod.zurita@gmail.com>
2017-05-04 11:09:08 -0400

    Footer and Header files created

---
+++ View/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+<title>Tasker</title>
+<link rel="stylesheet" type="text/css"
+	   href="/is218_FinalProj/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Tasker</h1></header>
