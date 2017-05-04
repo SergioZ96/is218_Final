00a519b2b47684cc50a2f7e268c3ae913f769a55
Sergio Zurita <Sergiod.zurita@gmail.com>
2017-05-04 14:26:02 -0400

    Acquiring info for registration page/making required fields

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-
+	require 'DBConfig/config.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,30 +14,57 @@
 
 	
 
-	<form class="myform" action="index.php" method="post">
+	<form class="myform" action="register.php" method="post">
 		<label><b>First:</label><br>
-		<input type="text" class="inputvalues" placeholder="Type your First Name"/><br>
+		<input name="firstname" type="text" class="inputvalues" placeholder="Type your First Name" required/><br>
 
 		<label><b>Last:</label><br>
-		<input type="text" class="inputvalues" placeholder="Type your Last Name"/><br>
+		<input name="lastname" type="text" class="inputvalues" placeholder="Type your Last Name" required/><br>
 
 		<label><b>E-Mail:</label><br>
-		<input type="email" class="inputvalues" placeholder="Type your E-Mail Address"/><br>
+		<input name="useremail" type="email" class="inputvalues" placeholder="Type your E-Mail Address" required/><br>
 
 		<label><b>Phone Number:</label><br>
-		<input type="number" class="inputvalues" placeholder="Type your Phone Number"/><br>
+		<input name="phnumber" type="number" class="inputvalues" placeholder="Type your Phone Number" required/><br>
 
 		<label><b>Birthday:</label><br>
-		<input type="date" class="inputvalues" placeholder="Type your Birthday"/><br>
+		<input name="birthday" type="date" class="inputvalues" placeholder="Type your Birthday" required/><br>
 
 		<label><b>Gender:</label><br>
-		<input type="radio" name="gender" value="m"> Male<br>
-		<input type="radio" name="gender" value="f"> Female<br>
+		<input type="radio" name="gender" value="m" > Male<br>
+		<input type="radio" name="gender" value="f" > Female<br>
 
 
-		<input type="submit" id="signup_btn" value="Sign Up"/><br>
+		<input name="submit_btn" type="submit" id="signup_btn" value="Sign Up"/><br>
 		<input type="button" id="back_btn" value="Back"/>
 	</form>
+
+	<?php
+		if(isset($_POST['submit_btn']))
+		{
+			//echo '<script type="text/javascript"> alert("Sign Up button clicked") </script>';
+
+			$firstname = $_POST['firstname'];
+			$lastname = $_POST['lastname'];
+			$useremail = $_POST['useremail'];
+			$phnumber = $_POST['phnumber'];
+			$birthday = $_POST['birthday'];
+			$gender = $_POST['gender'];
+
+			global $db;
+			$query="'SELECT * FROM UsersAccount 
+					WHERE Email='$useremail'";
+
+			$query_run=$db->prepare($query);
+			$num_rows = $query_run->fetchColumn();
+			if($num_rows > 0)
+			{
+				//there is already a user with the same username
+				echo '<script type="text/javascript"> alert("Email is already being used. Try another email.") </script>';
+			}
+
+		}
+	?>
 </div>
 
 </body>
